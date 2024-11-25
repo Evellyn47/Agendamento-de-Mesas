9c688c56f64207dd532f8047ac2170316225c8a6
Evellyn <cabral.evellyn1@gmail.com>
2024-11-25 14:24:07 -0300

    Entendendo o funcionamento do codigo

---
+++ restaurante.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+
+class Restaurante
+{
+ constructor() 
+  {
+    const reservarMesa = [];
+  }
+  reservarMesa ()
+  {
+    console.log("")
+    
+  }
+
+
+
+
+}
